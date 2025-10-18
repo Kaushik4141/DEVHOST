7f23b999bda01166c8e1f4b612ebd17e6d112abe
Kaushik H S <kaushik0h0s@gmail.com>
2025-10-18 13:12:04 +0530

    changi the api endpoint for community page

--- frontend/src/libs/api.ts
+++ frontend/src/libs/api.ts
@@ -1,4 +1,4 @@
-export const API_BASE = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:4000";
+export const API_BASE = process.env.BACKEND_API_URL || "http://localhost:4000";
 
 async function apiFetch<T = any>(path: string, init: RequestInit = {}): Promise<T> {
   const res = await fetch(`${API_BASE}${path}`, {
